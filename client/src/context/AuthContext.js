8441f52821dbe1c4941fa124e430afdf776222d4
PraveenKumarSuggula <praveenkumar.suggula0@gmail.com>
2025-03-19 02:37:33 -0500

    Implemented authentication context

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState, createContext } from 'react';
+import { auth } from '../config/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
