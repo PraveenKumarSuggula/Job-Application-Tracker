8d26ec642276d84fbb6115be7295fecf34325464
PraveenKumarSuggula <praveenkumar.suggula0@gmail.com>
2025-03-19 02:42:04 -0500

    UI & Styling Improvements

---
+++ client/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import JobForm from '../components/JobForm';
+import JobGrid from '../components/JobGrid';
+import { Button, Container, Typography } from '@mui/material';
+import { signOut } from 'firebase/auth';
+import { auth } from '../config/firebase';
+import JobTable from '../components/JobTable';
+
+const Home = () => (
+  <Container maxWidth="lg" sx={{ my: 4 }}>
+    <Button variant="outlined" color="error" onClick={() => signOut(auth)} sx={{ mb: 2 }}>
+      Logout
+    </Button>
+
+    <JobForm />
+
+    <Typography variant="h6" sx={{ mt: 6, mb: 2 }}>Your Job Applications:</Typography>
+    <JobTable />
+  </Container>
+);
+
+export default Home;
