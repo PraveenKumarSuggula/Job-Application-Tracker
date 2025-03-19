98ff287fdf7afc1ebaba171b9b9b9cf6a8b8d3c0
PraveenKumarSuggula <praveenkumar.suggula0@gmail.com>
2025-03-19 02:43:25 -0500

    firebase API setup

---
+++ client/src/config/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+export { auth, db, googleProvider, facebookProvider };
